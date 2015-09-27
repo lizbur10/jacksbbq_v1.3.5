3169c511419427ba70d82e9d19d910088d161510
Liz Burton <liz.burton@comcast.net>
2015-09-27 08:52:47 -0400

    Formatting fixes
    Modified to limit formatting changes to front page

--- content.php
+++ content.php
@@ -13,17 +13,21 @@ if ( get_post_type() === 'events' ) {
 
 <article class="space-bottom">
 
-	<div class="inside-stuff">
-		<?php // Blog & Index Supporting Image ?>
-		<?php if ( ( is_home() || is_search() || is_archive() ) && !is_post_type_archive('events') && has_post_thumbnail() ) {
-			$img = get_the_post_thumbnail(
-				$post->ID,
-				'medium',
-				array(
-					'class'	=> 'img-fullbleed'
-				)
-			);
-		?>
+	<?php if ( is_front_page() ) : ?>
+		<div class="inside-stuff">
+	<?php else : ?>
+		<div>
+	<?php endif; 
+			  // Blog & Index Supporting Image ?>
+			<?php if ( ( is_home() || is_search() || is_archive() ) && !is_post_type_archive('events') && has_post_thumbnail() ) {
+				$img = get_the_post_thumbnail(
+					$post->ID,
+					'medium',
+					array(
+						'class'	=> 'img-fullbleed'
+					)
+				); ?>
+
 			<aside class="grid-third grid-flip">
 				<p><?php echo $img; ?></p>
 			</aside>
@@ -46,8 +50,10 @@ if ( get_post_type() === 'events' ) {
 			<div class="grid-full">
 		<?php elseif ( get_post_type() === 'events' ) : ?>
 			<div class="grid-half">
-		<?php elseif ( is_home() || is_search() || is_archive() || is_front_page() ) : ?>
+		<?php elseif ( is_front_page() ) : ?>
 			<div class="grid-60pct inline-block">
+		<?php elseif ( is_home() || is_search() || is_archive() ) : ?>
+			<div class="grid-two-thirds inline-block">
 		<?php else : ?>
 			<div class="grid-two-thirds float-center">
 		<?php endif; ?>
@@ -123,62 +129,64 @@ if ( get_post_type() === 'events' ) {
 		<?php endif; ?>
 	</div>
 
+<?php if ( is_front_page() ) : ?>
 
-<div class="signup">
-<!--Begin CTCT Sign-Up Form-->
-<!-- EFD 1.0.0 [Thu Jul 23 09:49:45 EDT 2015] -->
-<link rel='stylesheet' type='text/css' href='https://static-pre-prod.ctctcdn.com/h/contacts-embedded-signup-assets/0.0.1-master-SNAPSHOT/css/signup-form.css'>
-<div class="ctct-embed-signup" style="font: 16px Open-Sans, Helvetica Neue, Arial, sans-serif; font: 1rem Helvetica Neue, Arial, sans-serif; line-height: 1.5; -webkit-font-smoothing: antialiased;">
-   <div style="color:#6D3429; background-color:#e8e8e8; border-radius:5px;">
-       <span id="success_message" style="display:none;">
-           <div style="text-align:center;">Thanks for signing up!</div>
-       </span>
+	<div class="signup">
+	<!--Begin CTCT Sign-Up Form-->
+	<!-- EFD 1.0.0 [Thu Jul 23 09:49:45 EDT 2015] -->
+	<link rel='stylesheet' type='text/css' href='https://static-pre-prod.ctctcdn.com/h/contacts-embedded-signup-assets/0.0.1-master-SNAPSHOT/css/signup-form.css'>
+	<div class="ctct-embed-signup" style="font: 16px Open-Sans, Helvetica Neue, Arial, sans-serif; font: 1rem Helvetica Neue, Arial, sans-serif; line-height: 1.5; -webkit-font-smoothing: antialiased;">
+	   <div style="color:#6D3429; background-color:#e8e8e8; border-radius:5px;">
+	       <span id="success_message" style="display:none;">
+	           <div style="text-align:center;">Thanks for signing up!</div>
+	       </span>
 
-       <form data-id="embedded_signup:form" class="ctct-custom-form Form" name="embedded_signup" method="POST" action="https://visitor2.d1.constantcontact.com/api/signup">
-           <h2 style="margin:0; text-transform: capitalize; font-size: 150%; font-weight: bold; padding-bottom: 0.5em; padding-top: 0.5em;">Stay In Touch</h2>
-           <p>Never miss out on your favorite limited edition sandwich again!</p>
-           <!-- The following code must be included to ensure your sign-up form works properly. -->
-           <input data-id="ca:input" type="hidden" name="ca" value="5cd3a851-97dd-4c71-9a38-b5d293e7ac03">
-           <input data-id="source:input" type="hidden" name="source" value="EFD">
-           <input data-id="required:input" type="hidden" name="required" value="list,email">
-           <input data-id="url:input" type="hidden" name="url" value="">
+	       <form data-id="embedded_signup:form" class="ctct-custom-form Form" name="embedded_signup" method="POST" action="https://visitor2.d1.constantcontact.com/api/signup">
+	           <h2 style="margin:0; text-transform: capitalize; font-size: 150%; font-weight: bold; padding-bottom: 0.5em; padding-top: 0.5em;">Stay In Touch</h2>
+	           <p>Never miss out on your favorite limited edition sandwich again!</p>
+	           <!-- The following code must be included to ensure your sign-up form works properly. -->
+	           <input data-id="ca:input" type="hidden" name="ca" value="5cd3a851-97dd-4c71-9a38-b5d293e7ac03">
+	           <input data-id="source:input" type="hidden" name="source" value="EFD">
+	           <input data-id="required:input" type="hidden" name="required" value="list,email">
+	           <input data-id="url:input" type="hidden" name="url" value="">
 
-           <p data-id="Email Address:p" ><label data-id="Email Address:label" data-name="email" class="ctct-form-required">Email Address</label> <input data-id="Email Address:input" type="text" name="email" value="" maxlength="80"></p>
-           
-           <p data-id="First Name:p" ><label data-id="First Name:label" data-name="first_name">First Name</label> <input data-id="First Name:input" type="text" name="first_name" value="" maxlength="50"></p>
-           
-           <p data-id="Birthday:p" ><label data-id="Birthday:label" data-name="birthday_day,birthday_month">Birthday</label> <input data-id="Birthday:input" type="text" name="birthday_month" value="" placeholder="MM" style="width:75px; display: inline;" maxlength="2"> <span style="margin: .5em;"> / </span><input data-id="Birthday:input" type="text" name="birthday_day" value="" placeholder="DD" style="width:75px; display: inline;" maxlength="2"></p>
-           
-              <p data-id="Lists:p" ><label data-id="Lists:label" data-name="list" class="ctct-form-required">Email Lists</label><div><input data-id="Lists:input" type="checkbox" name="list_0" value="1824530898"><span data-id="Lists:span" style="font-size: 95%;">Jack's BBQ Birthday Coupons</span></div><div><input data-id="Lists:input" type="checkbox" name="list_1" value="1374614046"><span data-id="Lists:span" style="font-size: 95%;">Jack's BBQ Food Truck</span></div><div><input data-id="Lists:input" type="checkbox" name="list_2" value="1584259900"><span data-id="Lists:span" style="font-size: 95%;">Jack's BBQ Menu &amp; Specials</span></div><div><input data-id="Lists:input" type="checkbox" name="list_3" value="1994393936"><span data-id="Lists:span" style="font-size: 95%;">Jack's BBQ Special Events</span></div></p>
+	           <p data-id="Email Address:p" ><label data-id="Email Address:label" data-name="email" class="ctct-form-required">Email Address</label> <input data-id="Email Address:input" type="text" name="email" value="" maxlength="80"></p>
+	           
+	           <p data-id="First Name:p" ><label data-id="First Name:label" data-name="first_name">First Name</label> <input data-id="First Name:input" type="text" name="first_name" value="" maxlength="50"></p>
+	           
+	           <p data-id="Birthday:p" ><label data-id="Birthday:label" data-name="birthday_day,birthday_month">Birthday</label> <input data-id="Birthday:input" type="text" name="birthday_month" value="" placeholder="MM" style="width:75px; display: inline;" maxlength="2"> <span style="margin: .5em;"> / </span><input data-id="Birthday:input" type="text" name="birthday_day" value="" placeholder="DD" style="width:75px; display: inline;" maxlength="2"></p>
+	           
+	              <p data-id="Lists:p" ><label data-id="Lists:label" data-name="list" class="ctct-form-required">Email Lists</label><div><input data-id="Lists:input" type="checkbox" name="list_0" value="1824530898"><span data-id="Lists:span" style="font-size: 95%;">Jack's BBQ Birthday Coupons</span></div><div><input data-id="Lists:input" type="checkbox" name="list_1" value="1374614046"><span data-id="Lists:span" style="font-size: 95%;">Jack's BBQ Food Truck</span></div><div><input data-id="Lists:input" type="checkbox" name="list_2" value="1584259900"><span data-id="Lists:span" style="font-size: 95%;">Jack's BBQ Menu &amp; Specials</span></div><div><input data-id="Lists:input" type="checkbox" name="list_3" value="1994393936"><span data-id="Lists:span" style="font-size: 95%;">Jack's BBQ Special Events</span></div></p>
 
-           <button type="submit" class="Button ctct-button Button--block Button-secondary" style="color: rgb(255, 255, 255); padding: 8px 10px; text-shadow: none; border-radius: 0px; background-color: rgb(246, 82, 29);">Sign Up</button>
+	           <button type="submit" class="Button ctct-button Button--block Button-secondary" style="color: rgb(255, 255, 255); padding: 8px 10px; text-shadow: none; border-radius: 0px; background-color: rgb(246, 82, 29);">Sign Up</button>
 
-       	<div><p class="ctct-form-footer">By submitting this form, you are granting: Jack's Backyard BBQ, 123 Main Street, Waltham, Massachusetts, 02451, United States, http://www.jacksbackyardbbq.com permission to email you. You may unsubscribe via the link found at the bottom of every email.  (See our <a href="http://www.constantcontact.com/legal/privacy-statement" target="_blank">Email Privacy Policy</a> for details.) Emails are serviced by Constant Contact.</p></div>
-       </form>
-   </div>
-</div>
-</div>
+	       	<div><p class="ctct-form-footer">By submitting this form, you are granting: Jack's Backyard BBQ, 123 Main Street, Waltham, Massachusetts, 02451, United States, http://www.jacksbackyardbbq.com permission to email you. You may unsubscribe via the link found at the bottom of every email.  (See our <a href="http://www.constantcontact.com/legal/privacy-statement" target="_blank">Email Privacy Policy</a> for details.) Emails are serviced by Constant Contact.</p></div>
+	       </form>
+	   </div>
+	</div>
+	</div>
 
-<script type='text/javascript'>
-   var localizedErrMap = {};
-   localizedErrMap['required'] = 		'This field is required.';
-   localizedErrMap['ca'] = 			'An unexpected error occurred while attempting to send email.';
-   localizedErrMap['email'] = 			'Please enter your email address in name@email.com format.';
-   localizedErrMap['birthday'] = 		'Please enter birthday in MM/DD format.';
-   localizedErrMap['anniversary'] = 	'Please enter anniversary in MM/DD/YYYY format.';
-   localizedErrMap['custom_date'] = 	'Please enter this date in MM/DD/YYYY format.';
-   localizedErrMap['list'] = 			'Please select at least one email list.';
-   localizedErrMap['generic'] = 		'This field is invalid.';
-   localizedErrMap['shared'] = 		'Sorry, we could not complete your sign-up. Please contact us to resolve this.';
-   localizedErrMap['state_mismatch'] = 'Mismatched State/Province and Country.';
-   localizedErrMap['state_province'] = 'Select a state/province';
-   localizedErrMap['selectcountry'] = 	'Select a country';
-   var postURL = 'https://visitor2.d1.constantcontact.com/api/signup';
-</script>
+	<script type='text/javascript'>
+	   var localizedErrMap = {};
+	   localizedErrMap['required'] = 		'This field is required.';
+	   localizedErrMap['ca'] = 			'An unexpected error occurred while attempting to send email.';
+	   localizedErrMap['email'] = 			'Please enter your email address in name@email.com format.';
+	   localizedErrMap['birthday'] = 		'Please enter birthday in MM/DD format.';
+	   localizedErrMap['anniversary'] = 	'Please enter anniversary in MM/DD/YYYY format.';
+	   localizedErrMap['custom_date'] = 	'Please enter this date in MM/DD/YYYY format.';
+	   localizedErrMap['list'] = 			'Please select at least one email list.';
+	   localizedErrMap['generic'] = 		'This field is invalid.';
+	   localizedErrMap['shared'] = 		'Sorry, we could not complete your sign-up. Please contact us to resolve this.';
+	   localizedErrMap['state_mismatch'] = 'Mismatched State/Province and Country.';
+	   localizedErrMap['state_province'] = 'Select a state/province';
+	   localizedErrMap['selectcountry'] = 	'Select a country';
+	   var postURL = 'https://visitor2.d1.constantcontact.com/api/signup';
+	</script>
 
-<script type='text/javascript' src='https://static-pre-prod.ctctcdn.com/h/contacts-embedded-signup-assets/0.0.1-master-SNAPSHOT/js/signup-form.js'></script>
-<!--End CTCT Sign-Up Form-->
+	<script type='text/javascript' src='https://static-pre-prod.ctctcdn.com/h/contacts-embedded-signup-assets/0.0.1-master-SNAPSHOT/js/signup-form.js'></script>
+	<!--End CTCT Sign-Up Form-->
 
+<?php endif; ?>
 </article>
 
 
